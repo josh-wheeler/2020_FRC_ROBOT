d866a61f98c01dc98dd6931f4cb7579f26e8ef91
josh wheeler <joshuawheeler87@gmail.com>
2020-02-02 14:59:55 -0500

    after encoder

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,7 +20,7 @@ import frc.robot.subsystems.DriveSubsystem;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
+ * documentation. If you change the name of this class or the package after  
  * creating this project, you must also update the build.gradle file in the
  * project.
  */
